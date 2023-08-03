0a86ecabfd75b3d3cb9fd7e1f362b86bbfa151b9
Bashorun407 <83551336+Bashorun407@users.noreply.github.com>
2023-08-03 19:39:39 +0100

    Committing all uploaded application files to local repository

--- book-transaction-service/src/main/java/com/akinnova/booktransactionservice/BookTransactionServiceApplication.java
+++ book-transaction-service/src/main/java/com/akinnova/booktransactionservice/BookTransactionServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableDiscoveryClient
 @EnableWebMvc
 public class BookTransactionServiceApplication {
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookTransactionServiceApplication.class, args);
